96d7560af4ca6412563b5cfcffb3c8768c717ae2
Per Terp <43138815+Oblivoin@users.noreply.github.com>
2023-02-01 13:55:10 +0100

    heartbeat works

--- PythonClient/Client.py
+++ PythonClient/Client.py
@@ -1,8 +1,10 @@
 import paho.mqtt.client as mqtt
 import time
+import select
+import json
 
 class Client:
-    def __init__(self, id):
+    def __init__(self, id, connectionTimeout=60):
         self.id = id
         self.broker = "localhost"
         self.port = 1883
@@ -11,16 +13,20 @@ class Client:
         self.response_topic = f"ddps/system/{self.id}/response"
         
         self.connect_topic = f"ddps/system/clientmanager/connect"
+        self.query_topicc = f"ddps/system/{self.id}/query"
 
+        self.connectionTimeout = connectionTimeout
         self.heartbeat_topic = f"ddps/system/{self.id}/heartbeat"
-        self.heartbeat_interval = 5
+        self.heartbeat_interval = 10
 
-        self.publish_id = ""
+        self.request_id = 1 # goes up when sending a message on query_topic 
         self.chyper = ""
         self.return_value = ""
         self.return_value = ""
 
         self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
 
     def print(self):
         print(self.heartbeat_topic)
@@ -29,26 +35,43 @@ class Client:
         self.client.connect(self.broker, self.port, 60)
 
         self.client.publish(self.connect_topic, self.id)
-    
-    def on_connect(self, client, userdata, flags, rc):
-        client.subscribe("income")
 
-    def on_message(client, userdata, msg):
-        print(f'Received message on topic {msg.topic}: {msg.payload}')
+    def handleResponse(self, response):
+        jsonResponse = response.split("<>")[1]
+        j = json.loads(jsonResponse)
+        heartbeatInterval = j["HeartbeatInterval"]
+        return heartbeatInterval
+
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload.decode('utf-8')
+        print(f'Received message on topic {msg.topic}: {payload}')
         if msg.topic == "income":
+            print("got it")
             client.publish("receive","got it!")
+        elif msg.topic == self.response_topic:
+            heartbeatInterval = self.handleResponse(payload)
+            self.heartbeat_interval = heartbeatInterval
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe("income")  
+        client.subscribe(self.response_topic)
+    
+
 
 if __name__ =="__main__":
     c = Client("temp4")
     c.print()
     c.connect_to_broker()
-    c.on_connect(c, userdata, flags, rc)
 
-    i = 0
-    while i < 100:
+    while True:
         c.client.loop()
-        c.client.publish("heartbeat", "beat")
-        time.sleep(5)
+        c.client.publish(c.heartbeat_topic, f"falmebeat")
 
-    c.client.disconnect()
+        ready = select.select([c.client._sock],[],[],c.heartbeat_interval)
+        if ready[0]:
+            c.client.loop() # handle incoming messages
+        else:
+            pass
+
     
--- PythonClient/test.py
+++ PythonClient/test.py
@@ -1,22 +1,18 @@
+from pydoc_data.topics import topics
 import paho.mqtt.client as mqtt
 import time
+import select
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("income")
 
-def on_message(client, userdata, msg):
-    print(f'Received message on topic {msg.topic}: {msg.payload}')
-    if msg.topic == "income":
-        client.publish("receive","got it!")
+json_data = {
+    "body": {
+        "temp": 30,
+        "heart": 80
+    },
+    "timestamp": "2023-01-27T12:00:00Z"
+}
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+heart_value = json_data["body"]["heart"]
+print(heart_value)
+# Output: 80
 
-client.connect("localhost", 1883, 60) # replace localhost with the IP address of your broker
-
-while True:
-    client.loop()
-    client.publish("heartbeat", "beat")
-    time.sleep(5)
