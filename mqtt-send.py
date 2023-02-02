9b562af11265f46a2b98fe5e62650f0396c21d24
Denmads <43139465+Denmads@users.noreply.github.com>
2023-02-02 16:14:14 +0100

    Added handy methods to mqtt test client

--- mqtt-send.py
+++ mqtt-send.py
@@ -1,3 +1,4 @@
+import json
 import paho.mqtt.client as mqtt
 
 # The callback for when a PUBLISH message is received from the server.
@@ -24,10 +25,39 @@ while mes != "quit":
     
     tokens = mes.split()
     
-    if tokens[0] == "subscribe":
+    if tokens[0] == "connect": #connect clientid
+        payload = {
+            "ClientId": tokens[1],
+            "ConnectionTimeout": 1000
+        }
+        client.publish("ddps/system/clientmanager/connect", f"connect<>{json.dumps(payload)}")
+    elif tokens[0] == "query" and tokens[1] == "publish": #query publish clientid type
+        payload = {
+            "RequestId":1,
+            "CypherQuery":f"MERGE(r:Room {{name:'my-room'}}) MERGE(s:Sensor {{type:'{tokens[3]}'}}) MERGE (r)-[:Contains]->(s)",
+            "TargetNode":"s",
+            "DataType":"float"
+        }
+        client.publish(f"ddps/system/{tokens[2]}/query", f"publish<>{json.dumps(payload)}")
+    elif tokens[0] == "query" and tokens[1] == "subscribe": #query subscribe clientid name:type name:type ...
+        targets = list(map(lambda x: x.split(":") ,tokens[3:]))
+        
+        query = "MATCH(r:Room {name:'my-room'})"
+        for trg in targets:
+            query += f"""
+                 MATCH ({trg[0]}:Sensor {{type:'{trg[1]}'}})
+                MATCH (r)-[:Contains]->({trg[0]})
+            """
+        
+        payload = {
+            "RequestId":1,
+            "CypherQuery": query,
+            "TargetNodes":list(map(lambda x: x[0] ,targets))}
+        client.publish(f"ddps/system/{tokens[2]}/query", f"subscribe<>{json.dumps(payload)}")
+    elif tokens[0] == "subscribe": #subscribe topic
         client.subscribe(tokens[1])
         print(f"Subscribe to '{tokens[1]}'")
-    else:
+    else: #publish topic data(can contain space)
         client.publish(tokens[1], " ".join(tokens[2:]))
         print(f"Published '{tokens[2]}'")
 
